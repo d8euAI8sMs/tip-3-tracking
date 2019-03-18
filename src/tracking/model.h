13ec5811c4015fa15e3dff79f582e3cbdc0e8d4e
kalaider <a.kalaider@yandex.ru>
2019-03-18 03:03:38 +0300

    feat: opencv-featured tracking
    -- use default opencv lucas-kanade tracker with opencv pivot selection
    algorithm
    -- much faster than previous implementation
    -- less pivot number (pivot distance and total number restriction)
    -- poor quality (bounding box collapses)

--- src/tracking/model.h
+++ src/tracking/model.h
@@ -214,275 +214,90 @@ namespace model
         {
         }
     public:
-        void good_features_to_track(
-            plot::world_t bb0,
-            const cv::Mat & frame0,
-            const cv::Mat & frame1,
-            std::vector < geom::point2d_t > & pivots,
-            const config & cfg) const
-        {
-            pivots.clear();
-
-            plot::screen_t bb = {
-                (int) std::ceil(bb0.xmin * frame0.cols) - 3,
-                (int) std::ceil(bb0.xmax * frame0.cols) + 3,
-                (int) std::ceil(bb0.ymin * frame0.rows) - 3,
-                (int) std::ceil(bb0.ymax * frame0.rows) + 3
-            };
-
-            if (bb.width() <= 0 || bb.height() <= 0) return;
-
-            std::vector < float > quality(bb.height() * bb.width());
-
-            float maxquality = 0;
-
-            for (size_t i = 0; i < bb.height(); ++i)
-            for (size_t j = 0; j < bb.width(); ++j)
-            {
-                geom::point2d_t pivot = {
-                    ((int) j + bb.xmin) / (float) frame0.cols,
-                    ((int) i + bb.ymin) / (float) frame0.rows
-                }, grad;
-                quality[i * bb.width() + j] = track(
-                    pivot, { 0, 0 }, frame0, frame1, grad, cfg);
-                maxquality = std::fmax(maxquality, quality[i * bb.width() + j]);
-            }
-
-            for (size_t i = 0; i < bb.height(); ++i)
-            for (size_t j = 0; j < bb.width(); ++j)
-            {
-                if (quality[i * bb.width() + j] < std::fmin(
-                        cfg.eigenvalue_threshold, maxquality * cfg.feature_threshold))
-                    continue;
-                pivots.emplace_back(
-                    ((int) j + bb.xmin) / (float) frame0.cols,
-                    ((int) i + bb.ymin) / (float) frame0.rows
-                );
-            }
-        }
         void track(
             std::vector < plot::world_t > & bb0,
             std::vector < std::pair < geom::point2d_t, geom::point2d_t > > & flow,
             const video_stream & s,
             const config & cfg) const
         {
-            size_t rc = s.video.frames[0].rows;
-            size_t cc = s.video.frames[0].cols;
+            auto ref = s.video.frames[s.frame].clone();
+            auto next = s.video.frames[s.frame + 1].clone();
 
-            size_t maxmip = (size_t) std::fmax(std::floor(std::log2(std::fmin(rc, cc))) - 4, 0);
-
-            size_t mip = std::fmin(maxmip, cfg.mip_count);
-
-            std::vector < std::pair < cv::Mat, cv::Mat > > mips(mip);
-
-            mips[0] = { s.video.frames[s.frame].clone(), s.video.frames[s.frame + 1].clone() };
-
-            for (size_t i = 1; i < mip; ++i)
-            {
-                cv::Size mipsize(cc / (1 << i), rc / (1 << i));
-                cv::resize(mips[0].first, mips[i].first, mipsize);
-                cv::resize(mips[0].second, mips[i].second, mipsize);
-            }
-
-            if (cfg.blur)
-            {
-                for (size_t i = 0; i < mip; ++i)
-                {
-                    cv::medianBlur(mips[i].first, mips[i].first, 5);
-                    cv::medianBlur(mips[i].second, mips[i].second, 5);
-                    cv::GaussianBlur(mips[i].first, mips[i].first, cv::Size(5, 5), 0);
-                    cv::GaussianBlur(mips[i].second, mips[i].second, cv::Size(5, 5), 0);
-                }
-            }
+            ref.convertTo(ref, CV_8U, 255);
+            next.convertTo(next, CV_8U, 255);
 
             flow.clear();
 
             for (size_t i = 0; i < bb0.size(); ++i)
             {
-                std::vector < geom::point2d_t > pivots;
+                plot::screen_t bb = {
+                    (int) std::ceil(bb0[i].xmin * ref.cols) - 5,
+                    (int) std::ceil(bb0[i].xmax * ref.cols) + 5,
+                    (int) std::ceil(bb0[i].ymin * ref.rows) - 5,
+                    (int) std::ceil(bb0[i].ymax * ref.rows) + 5
+                };
 
-                good_features_to_track(bb0[i], mips.front().first, mips.front().second, pivots, cfg);
+                if (bb.xmin < 0) bb.xmin = 0;
+                if (bb.ymin < 0) bb.ymin = 0;
+                if (bb.xmax >= ref.cols) bb.xmax = ref.cols - 1;
+                if (bb.ymax >= ref.rows) bb.ymax = ref.rows - 1;
 
-                std::vector < geom::point2d_t > grads(pivots.size());
-                std::vector < geom::point2d_t > tmpgrads(pivots.size());
-                std::vector < float > qual(pivots.size(), true);
-                std::vector < bool > state(pivots.size(), true);
+                if (bb.width() <= 5 || bb.height() <= 5) continue;
+               
+                std::vector<cv::Point2f> points[2];
+                cv::Mat status, err;
+                cv::goodFeaturesToTrack(
+                    ref.colRange(bb.xmin, bb.xmax).rowRange(bb.ymin, bb.ymax),
+                    points[0], 1000, cfg.feature_threshold, 1);
 
-                for (size_t m = 0; m < mip; ++m)
-                {
-                    track(pivots, tmpgrads, grads, qual, state,
-                          mips[mip - m - 1].first,
-                          mips[mip - m - 1].second, cfg);
-                }
+                if (points[0].size() < 5) continue;
+
+                cv::calcOpticalFlowPyrLK(
+                    ref.colRange(bb.xmin, bb.xmax).rowRange(bb.ymin, bb.ymax),
+                    next.colRange(bb.xmin, bb.xmax).rowRange(bb.ymin, bb.ymax),
+                    points[0], points[1], status, err);
 
                 plot::world_t bbn = {
                     10000, 0,
                     10000, 0,
                 };
 
-                for (size_t p = 0; p < pivots.size(); ++p)
+                for (size_t p = 0; p < points[0].size(); ++p)
                 {
-                    if (!state[p]) continue;
-                    auto pivot1 = pivots[p];
-                    auto pivot2 = pivots[p] + grads[p];
-
+                    if (status.at < std::uint8_t > (p, 0) == 0) continue;
+                    auto pivot1 = points[0][p];
+                    auto pivot2 = points[1][p];
+                    pivot1.x = bb0[i].xmin + pivot1.x / ref.cols;
+                    pivot1.y = bb0[i].ymin + pivot1.y / ref.rows;
+                    pivot2.x = bb0[i].xmin + pivot2.x / ref.cols;
+                    pivot2.y = bb0[i].ymin + pivot2.y / ref.rows;
+                
                     bbn.xmin = std::fmin(bbn.xmin, pivot1.x);
                     bbn.xmax = std::fmax(bbn.xmax, pivot1.x);
                     bbn.ymin = std::fmin(bbn.ymin, pivot1.y);
                     bbn.ymax = std::fmax(bbn.ymax, pivot1.y);
-
+                
                     bbn.xmin = std::fmin(bbn.xmin, pivot2.x);
                     bbn.xmax = std::fmax(bbn.xmax, pivot2.x);
                     bbn.ymin = std::fmin(bbn.ymin, pivot2.y);
                     bbn.ymax = std::fmax(bbn.ymax, pivot2.y);
                 }
-
-                bb0[i] = bbn;
-
-                for (size_t p = 0; p < pivots.size(); ++p)
-                {
-                    if (!state[p]) continue;
-                    flow.emplace_back(pivots[p], grads[p]);
-                }
-            }
-        }
-        void track(
-            const std::vector < geom::point2d_t > & pc0,
-            std::vector < geom::point2d_t > & tmpgrad,
-            std::vector < geom::point2d_t > & grad,
-            std::vector < float > & qual,
-            std::vector < bool > & state,
-            const cv::Mat & frame0,
-            const cv::Mat & frame1,
-            const config & cfg) const
-        {
-            geom::point2d_t newgrad;
-            float maxqual = 0;
-            for (size_t i = 0; i < pc0.size(); ++i)
-            {
-                if (!state[i]) continue;
-                qual[i] = track(pc0[i], grad[i], frame0, frame1, newgrad, cfg);
-                if (qual[i] < cfg.eigenvalue_threshold) state[i] = false;
-                tmpgrad[i] = grad[i] + newgrad;
-                maxqual = std::fmax(maxqual, qual[i]);
-            }
-            for (size_t i = 0; i < pc0.size(); ++i)
-            {
-                if (!state[i]) continue;
-                if (qual[i] < maxqual * cfg.feature_threshold)
-                {
-                    state[i] = false;
-                }
-                else
-                {
-                    grad[i] = tmpgrad[i];
-                }
-            }
-        }
-        float track(
-            const geom::point2d_t & pc0,
-            const geom::point2d_t & grad,
-            const cv::Mat & frame0,
-            const cv::Mat & frame1,
-            geom::point2d_t & out,
-            const config & cfg) const
-        {
-            size_t wndsize = 13;
-
-            float sx = wndsize / 3. / 2.;
-            float sy = wndsize / 3. / 2.;
-
-            cv::Mat wnd(wndsize, wndsize, CV_32F);
-
-            for (size_t i = 0; i <= wndsize / 2; ++i)
-            for (size_t j = 0; j <= wndsize / 2; ++j)
-            {
-                double d = std::exp(-1.0 * ((int)i * (int)i / sy / sy + (int)j * (int)j / sx / sx) / 2);
-                wnd.at < float > (wndsize / 2 - (int)i, wndsize / 2 - (int)j) = (float) d;
-                wnd.at < float > (wndsize / 2 + (int)i, wndsize / 2 + (int)j) = (float) d;
-                wnd.at < float > (wndsize / 2 - (int)i, wndsize / 2 + (int)j) = (float) d;
-                wnd.at < float > (wndsize / 2 + (int)i, wndsize / 2 - (int)j) = (float) d;
-            }
-
-            return track(pc0, grad, wnd, frame0, frame1, cfg, out);
-        }
-        float track(
-            const geom::point2d_t & pc0,
-            const geom::point2d_t & grad,
-            const cv::Mat & wnd,
-            const cv::Mat & frame0,
-            const cv::Mat & frame1,
-            const config & cfg,
-            geom::point2d_t & out) const
-        {
-            cv::Mat ref, next;
-
-            geom::point < int > pc = {
-                (int) std::round(pc0.x * frame0.cols),
-                (int) std::round(pc0.y * frame0.rows)
-            };
-            geom::point < int > pc2 = {
-                (int) std::round((pc0.x + grad.x) * frame0.cols),
-                (int) std::round((pc0.y + grad.y) * frame0.rows)
-            };
-
-            ref = frame0;
-            next = frame1;
-
-            cv::Mat a = cv::Mat::zeros(2, 2, CV_32FC1);
-            cv::Mat b = cv::Mat::zeros(2, 1, CV_32FC1);
-            
-            for (size_t i = 0; i < wnd.rows; ++i)
-            for (size_t j = 0; j < wnd.cols; ++j)
-            {
-                int r0 = pc.y - (int) i + wnd.rows / 2;
-                int c0 = pc.x - (int) j + wnd.cols / 2;
-                int r1 = pc2.y - (int) i + wnd.rows / 2;
-                int c1 = pc2.x - (int) j + wnd.cols / 2;
-                int ri[6] = { r0 - 1, r0, r0 + 1, r1 - 1, r1, r1 + 1 };
-                int ci[6] = { c0 - 1, c0, c0 + 1, c1 - 1, c1, c1 + 1 };
-                for (size_t k = 0; k < 6; ++k)
-                {
-                    if (ri[k] < 0) ri[k] = 0;
-                    if (ri[k] >= ref.rows) ri[k] = ref.rows - 1;
-                    if (ci[k] < 0) ci[k] = 0;
-                    if (ci[k] >= ref.cols) ci[k] = ref.cols - 1;
-                }
-                // sobel operator to calculate more stable gradients
-                float dy = (
-                    2 * (ref.at < float > (ri[2], ci[1]) - ref.at < float > (ri[0], ci[1])) +
-                    1 * (ref.at < float > (ri[2], ci[0]) - ref.at < float > (ri[0], ci[0])) +
-                    1 * (ref.at < float > (ri[2], ci[2]) - ref.at < float > (ri[0], ci[2]))
-                    ) / 4; /* possibly 8? */
-                float dx = (
-                    2 * (ref.at < float > (ri[1], ci[2]) - ref.at < float > (ri[1], ci[0])) +
-                    1 * (ref.at < float > (ri[0], ci[2]) - ref.at < float > (ri[0], ci[0])) +
-                    1 * (ref.at < float > (ri[2], ci[2]) - ref.at < float > (ri[2], ci[0]))
-                    ) / 4; /* possibly 8? */
-                float dt = next.at < float > (ri[3 + 1], ci[3 + 1]) - ref.at < float > (ri[1], ci[1]);
-                float wi = wnd.at < float > (i, j);
                 
-                a.at < float > (0, 0) += wi * dx * dx;
-                a.at < float > (1, 1) += wi * dy * dy;
-                a.at < float > (1, 0) += wi * dx * dy;
-                a.at < float > (0, 1) += wi * dx * dy;
-                b.at < float > (0, 0) += - wi * dx * dt;
-                b.at < float > (1, 0) += - wi * dy * dt;
+                bb0[i] = bbn;
+                
+                for (size_t p = 0; p < points[0].size(); ++p)
+                {
+                    auto pivot1 = points[0][p];
+                    auto pivot2 = points[1][p];
+                    pivot1.x = bb0[i].xmin + pivot1.x / ref.cols;
+                    pivot1.y = bb0[i].ymin + pivot1.y / ref.rows;
+                    pivot2.x = bb0[i].xmin + pivot2.x / ref.cols;
+                    pivot2.y = bb0[i].ymin + pivot2.y / ref.rows;
+                    if (status.at < std::uint8_t > (p, 0) == 0) continue;
+                    flow.emplace_back(geom::point2d_t{ pivot1.x, pivot1.y },
+                                      geom::point2d_t{ pivot2.x - pivot1.x, pivot2.y - pivot1.y });
+                }
             }
-
-            std::vector < float > eigen;
-            cv::eigen(a, eigen);
-
-            cv::invert(a, a, cv::DECOMP_SVD);
-
-            cv::Mat x = a * b;
-
-            float dx = x.at < float > (0) / frame0.cols;
-            float dy = x.at < float > (1) / frame0.rows;
-
-            out = { dx, dy };
-
-            return std::fmin(std::abs(eigen[0]), std::abs(eigen[1]));
         }
     };
 
